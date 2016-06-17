5a32523277a332f326ae053dc072443be0dbbbe4
lichenjie <1194222721@qq.com>
2016-06-16 22:31:15 -0700

    orm.py add some, not yet

--- www/orm.py
+++ www/orm.py
@@ -38,5 +38,46 @@ async def select(sql. args, size=None):
    logging,info('rows returned: %s' % len(rs))
    return rs
 
+async def execute(sql, args, size=None):
+  log(sql, args)
+  global __pool
+  async with __pool.get() as conn:
+    await with conn.cursor(aiomysql.DictCursor) as cur:
+      await cur.execute(sql.replace('?', '%s'), args)
+      affected = cur.rowcount
+    if not autocommit:
+      await.conn.commit()
+    except BaseException as e:
+      if not autocommit:
+        await conn.rollback()
+      raise
+    return affected
 
+def create_args_string(num):
+  L = []
+  for n in range(num):
+    L.append('?')
+  return ','.join(L)
+
+class Field(object):
+  def __init__(self, name, column_type, primary_key, default):
+    self.name = name
+    self.column_type = colume_type
+    self.primary_key = primary_key
+    self.default = default
+
+  def __str__(self):
+    return '<%s, %s:%s>' %(self.__class__.__name, self.column_type, self.name)
+
+class StringField(Field):
+  def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+    super().__init__(name, ddl, primary_key, default)
+
+class BooleanField(Field):
+  def __init__(self, name=None, default=False):
+    super().__init__(name, 'boolen', False, default)
+
+class IntegerField(Field):
+  def __init__(self, name=None, default=False)
+    super().__init__(name, '')
 
